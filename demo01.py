f021e975de4168711ee900353c100dc551e4dfa5
canghaimo <2284703194@qq.com>
2022-06-24 17:23:11 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,46 @@
+"""
+print("hello world") # 字符串
+print(2333)  # 整数
+print(2.333)  # 小数
+print(True)   # 布尔值 True False
+print(())   # 元组  
+print([])   #  数组
+print({})   #  字典
+
+锄禾日当午
+汗滴禾下锄
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")  #  字符串的拼接
+print("哈哈"*10)
+print(((1+2)*100-99)/2)
+print(2>3)
+
+变量
+赋值
+
+a ="张三"  #   把张三这个赋值给了变量a
+print(a)
+
+
+a =input("请输入：")
+b =input("请输入：")
+print("input获取的内容：",a+b)    #input只能识别字符串
+
+  #  数据格式的转换
+"""
+print(type("哈哈"))
+print(type(2333))
+print(type(2.333))
+print(type(()))
+print(type([]))
+print(type({}))
+#  a = float(input("请输入："))
+#  b = float(input("请输入："))
+#  print("input输入的内容:",a+b)
+
+a="asdkjsnckjdsnjn snckjdsnkjsnseuwnnv  wnc"
+print(len(a))
+"""
+练习：通过代码获取两段内容，并计算两段内容字符串长度之和。
+"""
---
+++ demo01lx.py
@@ -0,0 +1,46 @@
+"""
+print("哈哈")  # 字符串/str
+print(2333)  #  整数/int
+print(2.333)  #  小数/float
+print(True)  #  布尔值/bool
+print(())  #  元组/tuple
+print([])  #  数组/list
+print({})  #  字典/dict
+
+
+锄禾日当午
+汗滴禾下锄
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻") #  字符串的拼接
+print("哈哈"*10)
+print((((1+2)*100-99)/2))
+print(2>3)
+# 赋值 
+# 变量
+a="张三"   #  把张三赋值给变量a
+print(a)
+a=input("请输入：")
+b=input("请输入：")
+print("input获取的内容",a+b)  #  input只能识别字符串
+# 数据格式的转换
+print(type("哈哈"))
+print(type(2333))
+print(type(2.333))
+print(type())
+print(type[])
+print(type{})
+a=int(2.333)
+print(type(a))
+a=float(input("请输入："))
+b=float(input("请输入："))
+print("a+b之和：",a+b)
+a="aasjdscksdnckjdsnrnvks  jscnkjnvsus nsi"
+print(len(a))
+a=input("请输入：")
+b=input("请输入：")
+print(len(a+b))
+print(a+b)
+"""
+a=input("请输入：",)
+b=input("请输入：",)
+print(("a+b字符串长度之和",len(a+b)))
